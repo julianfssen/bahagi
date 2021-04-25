cd8e381235e7dfaaf25958881f743f0b6805797d
julianfssen <julianfssen@gmail.com>
2021-04-25 11:39:14 +0800

    Removed unnecessary console logs

--- index.ts
+++ index.ts
@@ -1,21 +1,19 @@
-import express from 'express';
-import Knex from 'knex';
-import { Model } from 'objection';
-import config from './src/config';
-import User from './src/models/user';
+import express from "express";
+import Knex from "knex";
+import { Model } from "objection";
+import config from "./src/config";
+import User from "./src/models/user";
 
 async function startServer() {
   const app = express();
 
-  await require('./src/loaders').default({ expressApp: app });
-  
-  app.get('/', (req, res) => {
-    res.send('Hello World!')
+  await require("./src/loaders").default({ expressApp: app });
+
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
   });
-  
-  app.listen(config.port, () => {
-    console.log(`Example app now running`)
-  });
+
+  app.listen(config.port, () => {});
 }
 
 startServer();
--- src/services/uploadService.ts
+++ src/services/uploadService.ts
@@ -31,12 +31,7 @@ export default class UploadService {
       postParams.ContentType = `${image.mimetype}`;
       const imageStream = createReadStream(image.path);
       postParams.Body = imageStream;
-      console.log("uploading to S3 now!");
-      console.log(process.env.AWS_BUCKET);
       const data = await s3.send(new PutObjectCommand(postParams));
-      if (data) {
-        console.log("got response from s3", data);
-      }
       unlink(image.path, (err) => {
         if (err) {
           throw err;
